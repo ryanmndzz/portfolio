63d89f9b53d1e2e87326a1d2ccaf43b0f27944d1
ryanmndzz <151802396+ryanmndzz@users.noreply.github.com>
2024-03-10 18:53:02 +0800

    almost done

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -2,16 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     particlesJS("hero_particles", {
         particles: {
             number: { value: 80, density: { enable: true, value_area: 800 } },
-            color: { value: "#ffffff" },
+            color: { value: "#18006d" },
             shape: {
                 type: "circle",
-                stroke: { width: 0, color: "#000000" },
+                stroke: { width: 0, color: "#18006d" },
                 polygon: { nb_sides: 5 },
                 image: { src: "img/github.svg", width: 100, height: 100 }
             },
             opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
             size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
-            line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.4, width: 1 },
+            line_linked: { enable: true, distance: 150, color: "#18006d", opacity: 0.4, width: 1 }, // Update this line to set the line color
             move: {
                 enable: true,
                 speed: 6,
